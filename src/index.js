8cba69a5dc83f4d993b3b5cacc9c718a6411aa3b
David Rangel <drangelalarcon@gmail.com>
2019-06-26 18:00:53 -0700

    first draft

---
+++ src/index.js
@@ -0,0 +1,36 @@
+function toggle(element, button) {
+    if (element.style.display == 'none') {
+        unfade(element);
+        button.innerHTML = "... hide"
+    }
+    else {
+        fade(element);
+        button.innerHTML = "... see more"
+    }
+}
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 15);
+}
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
